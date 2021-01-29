1e1b1a2d266b4f7708cdb67a84a731a4df2eac90
Arturo <evaglois@mx>
2021-01-29 03:03:12 -0600

    En el vid28 se agrego metodo chequeo_interno y se encapsulo el metodo

--- poo1_vid26.py
+++ poo1_vid26.py
@@ -11,15 +11,34 @@ class Coche():
 
     def arrancar(self, arrancamos):
         self.__enmarcha=arrancamos
+        
+        if(self.__enmarcha):
+            chequeo=self.__chequeo_interno()
 
-        if(self.__enmarcha==True):
+        if(self.__enmarcha and chequeo):
             return "El coche esta en  marcha"
+        
+        elif(self.__enmarcha and chequeo==False):
+            return "Algo ha ido mal en el chequeo no podemos arrancar"
+        
         else:
             return "El coche esta parado"
             
 
     def estado(self):
         print("El coche tiene ", self.__ruedas, "ruedas. Un ancho de ", self.__anchoChasis, " y un largo de ", self.__largoChasis)
+        
+    
+    def __chequeo_interno(self):
+        print("Realizando chequeo interno")
+        self.gasolina = "ok"
+        self.aceite="ok"
+        self.puertas="cerradas"
+        
+        if(self.aceite=="ok" and self.gasolina=="ok" and self.puertas=="cerradas"):
+            return True
+        else:
+            return False 
         
         
 
@@ -41,8 +60,6 @@ micoche2=Coche()
 
 
 print(micoche2.arrancar(False))
-
-micoche2.ruedas=2
 
 micoche2.estado()
 
